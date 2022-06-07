bd190b51ef115b7d2f4111cb4d4fe179ed0f954f
DevSeulgi <dev.dinobox@gmail.com>
2022-06-07 18:17:56 +0900

    Fix : 초기셋팅 수정

--- src/index.js
+++ src/index.js
@@ -1,9 +1,9 @@
 import { ThemeProvider } from 'styled-components';
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import Router from './Router.js';
-import GlobalStyles from './styles/GlobalStyles.js';
-import theme from './styles/theme.js';
+import Router from './Router';
+import GlobalStyles from './styles/GlobalStyles';
+import theme from './styles/theme';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
