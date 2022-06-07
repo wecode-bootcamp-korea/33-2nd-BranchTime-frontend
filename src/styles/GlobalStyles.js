713b9d9ca6fb0148065aa651ad0ca694aa2927f7
Hyeong kyeom Kim <78855917+Kyeom1997@users.noreply.github.com>
2022-06-07 22:25:08 +0900

    ADD: Main Footer / Main Component 레이아웃 및 styled component 구현
    Main Component 반복되는 데이터 export 코드 아래에 상수 데이터로 구현하여 map으로 관리
    Footer Component 레이아웃, hover시 effect 구현
    
    Modify: 1차 PR feedback 수정 완료
    
    1. map 함수 내 destructuring 완료
    2. react-icons 및 nesting tag styled-components 적용하여 수정 완료
    3. 상수 데이터 json 파일로 분리하여 fetch 완료
    
    Modify: 2차 PR Feedback 수정 완료
    
    1. destructuring 완료 및 return 삭제

--- src/styles/GlobalStyles.js
+++ src/styles/GlobalStyles.js
@@ -9,6 +9,28 @@ const GlobalStyles = createGlobalStyle`
   body {
       font-family: "Noto Sans KR", sans-serif;
   }
+
+  a {
+  text-decoration: none;
+  color: black;
+
+  &:visited {
+    text-decoration: none;
+  }
+
+  &:hover {
+    text-decoration: none;
+  }
+
+  &:focus {
+    text-decoration: none;
+  }
+
+  &:hover,
+  &:active {
+    text-decoration: none;
+  }
+}
 `;
 
 export default GlobalStyles;
