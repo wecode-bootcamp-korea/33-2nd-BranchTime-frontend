a07ab8a9a4ff858f246a838e3b8b3490828a4980
jooyoung <mirkduddl@naver.com>
2022-06-08 16:17:04 +0900

    Add : PostListBody - PostList
    - Post List card (이미지 없는) UI 구현

--- src/Router.js
+++ src/Router.js
@@ -1,12 +1,14 @@
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import React from 'react';
 import Main from './pages/main/Main';
+import PostListBody from './components/PostList/PostListBody';
 
 function Router() {
   return (
     <BrowserRouter>
       <Routes>
         <Route path="/" element={<Main />} />
+        <Route path="/post_list" element={<PostListBody />} />
       </Routes>
     </BrowserRouter>
   );
