cf6d98f396daba0d2f2f8d36fab81a02aa785863
DevSeulgi <dev.dinobox@gmail.com>
2022-06-08 18:02:27 +0900

    Add : MarkDown Editor 설정 및 Write 페이지 기능 구현
    - 마크다운 에디터 라이브러리 설치
    - 라이브러리 속성 값을 통해 Toolbar & InputBox Dragbar, 라이트 테마 설정
    - Edit Header UI 구현

--- src/Router.js
+++ src/Router.js
@@ -4,6 +4,7 @@ import Nav from './components/Nav/Nav';
 import Main from './pages/main/Main';
 import PostList from './pages/PostList/PostList';
 import PostDetail from './pages/PostDetail/PostDetail';
+import Write from './pages/write/Write';
 
 function Router() {
   return (
@@ -13,6 +14,7 @@ function Router() {
         <Route path="/main" element={<Main />} />
         <Route path="/post_list" element={<PostList />} />
         <Route path="/post_detail" element={<PostDetail />} />
+        <Route path="/write" element={<Write />} />
       </Routes>
     </BrowserRouter>
   );
