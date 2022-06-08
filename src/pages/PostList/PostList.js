4d90b7646ca46e4873e1fd0f2b80ef6970fb8b27
jooyoung <mirkduddl@naver.com>
2022-06-08 10:01:16 +0900

    Add : postList Header UI 구현
    - Styled-components를 사용해서 Title, SubCategories 스타일 작업
    - theme.js 내용 추가
    
    Modify : 리뷰 반영

---
+++ src/pages/PostList/PostList.js
@@ -0,0 +1,8 @@
+import React from 'react';
+import PostListHeader from '../../components/PostList/PostListHeader';
+
+const PostList = () => {
+  return <PostListHeader />;
+};
+
+export default PostList;
