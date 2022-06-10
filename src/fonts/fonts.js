a87009666b889d56da1feb2cb0cc986dfe41ecbb
jooyoung <mirkduddl@naver.com>
2022-06-10 15:18:37 +0900

    Add : PostDetailPage - Recommend 컴포넌트 생성 - Styled-components와 theme을 사용해서  UI 구현 - PostRecommend.json 목 데이터 생성 - 생성 한 목 데이터로 반복되는 UI map method로 구현 - px -> rem 단위 변경 - Nanum Font 추가 -> h1(Recommend card title)에 적용

--- src/fonts/fonts.js
+++ src/fonts/fonts.js
@@ -1,8 +1,13 @@
 import { createGlobalStyle } from 'styled-components';
 import ItalianStyleLtIt from './ItalianStyleLtIt.woff';
+import NanumMyeongjo from './NanumMyeongjo.ttf';
 
 export default createGlobalStyle`
 @font-face {
   font-family: "ItalianStyleLtIt";
-  src:url(${ItalianStyleLtIt}) format("woff")}
+  src:url(${ItalianStyleLtIt}) format("woff")};
+
+@font-face {
+font-family: "NanumMyeongjo";
+src:url(${NanumMyeongjo}) format("ttf")};
 `;
