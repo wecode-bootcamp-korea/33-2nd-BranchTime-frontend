0b594aa82303ea532f6880eacf1d04724f023053
Hyeong kyeom Kim <78855917+Kyeom1997@users.noreply.github.com>
2022-06-16 10:52:40 +0900

    ADD: main component Keyword List component 분리
    Modify: Conflict Resolve
    
    Modify: keyword list data, 백엔드와 통신으로 Fetch 후에 map으로 구현
    
    1. fetch로 데이터 state에 저장 후 map으로 keyword list 구현
    2. useNavigate() 함수를 통해 path parameter로 post_list component로 각 id값에 맞게 이동
    
    Modify: fetch ip config.js에서 import로 사용

--- src/pages/main/keyword/Keyword.js
+++ src/pages/main/keyword/Keyword.js
@@ -1,12 +1,33 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import styled from 'styled-components';
+import { useNavigate } from 'react-router-dom';
+import { BASE_URL } from '../../../config';
 
 const Keyword = () => {
+  const [keywords, setKeywords] = useState([]);
+
+  useEffect(() => {
+    fetch(`${BASE_URL}contents/`)
+      .then(res => res.json())
+      .then(data => setKeywords(data.message));
+  }, []);
+
+  const navigate = useNavigate();
+
+  const goToPostlist = id => {
+    navigate(`/post_list/${id}`);
+  };
+
   return (
     <KeywordList>
-      {KEYWORD_ARRAY.map(arr => {
-        const { id, name } = arr;
-        return <KeywordOne key={id}>{name}</KeywordOne>;
+      {keywords.slice(0, 24).map(keyword => {
+        const { id, main_category_name } = keyword;
+
+        return (
+          <KeywordOne onClick={() => goToPostlist(id)} key={id}>
+            {main_category_name}
+          </KeywordOne>
+        );
       })}
     </KeywordList>
   );
@@ -23,7 +44,7 @@ const KeywordList = styled.div`
   /* border: 1px solid black; */
 `;
 
-const KeywordOne = styled.a`
+const KeywordOne = styled.span`
   width: 7.438rem;
   height: 7.438rem;
   ${theme => theme.theme.flex.flexBox()}
@@ -36,102 +57,3 @@ const KeywordOne = styled.a`
     cursor: pointer;
   }
 `;
-
-const KEYWORD_ARRAY = [
-  {
-    id: 1,
-    name: '개발 . 프로그래밍',
-  },
-  {
-    id: 2,
-    name: '웹 개발',
-  },
-  {
-    id: 3,
-    name: '프론트엔드',
-  },
-  {
-    id: 4,
-    name: '백엔드',
-  },
-  {
-    id: 5,
-    name: '풀스택',
-  },
-  {
-    id: 6,
-    name: '모바일 앱 개발',
-  },
-  {
-    id: 7,
-    name: '프로그래밍 언어',
-  },
-  {
-    id: 8,
-    name: `알고리즘`,
-  },
-  {
-    id: 9,
-    name: '데이터 사이언스',
-  },
-  {
-    id: 10,
-    name: '데이터베이스',
-  },
-  {
-    id: 11,
-    name: '개발 도구',
-  },
-  {
-    id: 12,
-    name: '개발자 이야기',
-  },
-  {
-    id: 13,
-    name: '게임 개발',
-  },
-  {
-    id: 14,
-    name: '인공지능',
-  },
-  {
-    id: 15,
-    name: 'CAD. 3D 모델링',
-  },
-  {
-    id: 16,
-    name: 'UX/UI',
-  },
-  {
-    id: 17,
-    name: '사진, 영상',
-  },
-  {
-    id: 18,
-    name: '사운드',
-  },
-  {
-    id: 19,
-    name: '오피스',
-  },
-  {
-    id: 20,
-    name: '마케팅',
-  },
-  {
-    id: 21,
-    name: '업무 자동화',
-  },
-  {
-    id: 22,
-    name: '취업. 이직',
-  },
-  {
-    id: 23,
-    name: '개인 브랜딩',
-  },
-  {
-    id: 24,
-    name: '창업',
-  },
-];
